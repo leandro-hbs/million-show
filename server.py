c1e8a44a1b12b584777b600280a39d4e846c13b5
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2020-01-03 15:35:52 -0300

    Novas Questoes

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from random import randint
 
 global RANKING, PERGUNTAS, RESPOSTAS, ALTERNATIVAS
 
-RANKING = [('Leandro',1000000),('Norronha',5000)]
+RANKING = [('Leandro',1000000),('Noronha',900000)]
 PERGUNTAS = [
 'Sobre o protocolo DNS no modelo TCP/IP, é correto afirmar que: ',
 'O protocolo de transmissão que permite trocas de arquivos grandes e permite também acessar remotamente sistemas de arquivos, diretamente entre computadores sem passar pela web, é chamado: ',
@@ -14,20 +14,40 @@ PERGUNTAS = [
 'A configuração automática de endereços IP e outras informações da rede nas estações de trabalho é provida por um servidor DHCP (Dynamic Host Configuration Protocol). Assinale a alternativa que NÃO corresponde a uma informação fornecida por este servidor',
 'Seu gerente verificou uma irregularidade no tráfego HTTP e HTTPS em sua rede corporativa e solicita que estes serviços sejam bloqueados imediatamente através do sistema de firewall, sendo assim, as seguintes portas deverão ter o acesso bloqueado:',
 'Qual é o protocolo de internet para transferência segura, com uso de certificado digital, utilizado em sites de compras eletrônicas?',
-'Na Internet, para evitar que o tráfego de dados entre os usuários e seus servidores seja visualizado por terceiros, alguns sites, como os de bancos e de comércio eletrônico, utilizam em suas conexões o protocolo'
+'Na Internet, para evitar que o tráfego de dados entre os usuários e seus servidores seja visualizado por terceiros, alguns sites, como os de bancos e de comércio eletrônico, utilizam em suas conexões o protocolo',
+'Qual estrutura no globo ocular responsável por transformar a luz em impulso elétrico para o cérebro?',
+'Qual estrutura do ouvido interno é responsável por transformar impulsos de pressão em impulsos elétricos para o cérebro?',
+'A paixão (estado "hipermotivacional" de demência temporária) que do ponto de vista cerebral é caracterizada por: ',
+'O que ocorre no cérebro ao ingerirmos alcool?',
+'É um efeito e um causa da febre, EXCETO.',
+'"A vida deve estar em harmonia com o cosmos e maximizando suas virtudes e talentos", "Ame o mundo como ele é e dispense ideais","Todos temos uma potência, uma energia vital que oscila, felicidade é o ganho de potência", esses conceitos para felicidade são de, respectivamente.',
+'"Aja de forma que qualquer pessoa no seu lugar possa fazer exatamente o que você está fazendo", esta frase define:',
+'Em qual ordem desenvolvemos os 5 sentidos?',
+'Como o cérebro armazena informações?',
+'A afirmação "1 = 0.99999.." é:'
 ]
-RESPOSTAS = ['B','B','A','C', 'C', 'C', 'B', 'A', 'B', 'C']
+RESPOSTAS = ['B','B','A','C', 'C', 'C', 'B', 'A', 'B', 'C', 'A', 'B', 'A', 'C', 'B', 'A', 'B', 'A', 'C', 'B']
 ALTERNATIVAS = [
 'A) Utiliza somente UDP para resolução dos nomes\nB) Um resolver gerencia as buscas do cliente em um servidor DNS\nC) Um nome de domínio tem somente uma parte, chamada também de label\n',
 'A) HTTP\nB) SMTP\nC) FTP\n',
 'A) TLS\nB) ICMP\nC) NAT\n',
 'A) Padrão da internet para o gerenciamento de dispositivos em redes IP\nB) Para operação de serviços de rede de forma segura que utiliza criptografia\nC) Que permite a atribuição manual e a atribuição automática de endereços IP\n',
 'A) IP, TELNET, NFS\nB) ICMP, IP e DNS\nC) DHCP, DNS e SNMP\n',
-'A) Tradução dos nomes dos hospedeiros para endereços IP\nB) Apelidos de hospedeiros\nC) Resolução ARP (Protocolo de Resolução de Endereços)',
-'A) A máscara de sub-rede\nB) O endereço dos servidores Web\nC) O endereço dos servidores DNS',
-'A) 80 e 443\nB) 8080 e 443\nC) 80 e 81',
-'A) IMAP\nB) HTTPS\nC) SNMP',
-'A) FTP\nB) SMTP\nC) HTTPS'
+'A) Tradução dos nomes dos hospedeiros para endereços IP\nB) Apelidos de hospedeiros\nC) Resolução ARP (Protocolo de Resolução de Endereços)\n',
+'A) A máscara de sub-rede\nB) O endereço dos servidores Web\nC) O endereço dos servidores DNS\n',
+'A) 80 e 443\nB) 8080 e 443\nC) 80 e 81\n',
+'A) IMAP\nB) HTTPS\nC) SNMP\n',
+'A) FTP\nB) SMTP\nC) HTTPS\n',
+'A) Bastonetes\nB) Retina\nC) Cristalino\n',
+'A) Timpano\nB) Coclea\nC) Estribo\n',
+'A) Aumento da Dopamina (neurotransmissor que regula motivação e prazer)\nB) Aumento da Serotonina (neurotransmissor que regula o sono e apetite)\nC) Diminuição do cortisol (hormonio que regula o estresse)\n',
+'A) O alcool libera tóxinas responsáveis por um estímulo do sistema límbico\nB) O alcool estimula a parte emotiva mais primitiva do cérebro (libido)\nC) Os gases da reação quimica na ingestão inibem o cortex frontal do cérebro\n',
+'A) Vasoconstrição e protozoários\nB) Diminuição da temperatura corporal e vírus\nC) Tremores e bactérias\n',
+'A) Aristoteles, Nietzche e Spinoza\nB) Nietzche, Aristoteles e Spinoza\nC) Spinoza, Nietzche e Aristoteles\n',
+'A) O Amor Fati de Nietzche\nB) O Imperativo Categorico de Kant\nC) A Virtù de Maquiavél\n',
+'A) Tato, Audição, Paladar, Olfato, Visão\nB) Audição, Tato, Olfato, Paladar e Visão\nC) Tato, Olfato, Audição, Paladar e Visão\n',
+'A) Os neurônios guardam os impulsos nervosos e replicam quando necessario\nB) Os neurônios levam os impulsos pro subconsciente e quando necessário tornam consciente\nC) Os neurônios formam ligações entre si que será interpretada pelo cérebro quando necessário\n',
+'A) Falsa, pois claramente são strings diferentes\nB) Verdadeira, pois a fração geratriz de 0.9999.. resulta em 1\nC) Falsa, pois 1 é igual a 1 e 0.999... é igual a 0.999...\n'
 ]
 
 def interface(nickname, pontuacao, id, pular, chances):
@@ -70,34 +90,38 @@ class MultiplasExecucoes(threading.Thread):
         cont = 0
         acumulador = 1
         marcador = []
-        while chances > 0 and cont < 10:
-            while self.id in marcador:
-                self.id = randint(0,9)
-            marcador.append(self.id)
-            mensagem = interface(self.nickname, self.pontuacao, self.id, self.pular, chances)
-            self.conexao.send(str.encode(mensagem))
-            resposta = str(self.conexao.recv(1024), 'utf-8')
-            if resposta.upper() == 'PULAR':
-                cont += 1
-                if self.pular == 0:
-                    chances -= 1
-                    continue
+        while chances > 0:
+                while self.id in marcador:
+                    if cont < 10:
+                        self.id = randint(0,9)
+                    else:
+                        self.id = randint(10,20)
+                marcador.append(self.id)
+                mensagem = interface(self.nickname, self.pontuacao, self.id, self.pular, chances)
+                self.conexao.send(str.encode(mensagem))
+                resposta = str(self.conexao.recv(1024), 'utf-8')
+                if resposta.upper() == 'PULAR':
+                    cont += 1
+                    if self.pular == 0:
+                        chances -= 1
+                        continue
+                    else:
+                        self.pular -= 1
+                        continue
+                elif resposta.upper() == RESPOSTAS[self.id]:
+                    self.pontuacao += 1000 * acumulador
+                    acumulador += 1
                 else:
-                    self.pular -= 1
-                    continue
-            elif resposta.upper() == RESPOSTAS[self.id]:
-                self.pontuacao += 1000 * acumulador
-                acumulador += 1
-            else:
-                chances -= 1
-                acumulador = 1
-            cont += 1
+                    chances -= 1
+                    acumulador = 1
+                cont += 1
+
 
         RANKING.append((self.nickname,self.pontuacao))
 
         if chances == 0:
             self.pontuacao = 0
-            situacao = 'Você foi eliminado... mt noob'
+            situacao = 'Você foi eliminado'
         elif chances > 0:
             situacao = 'Você ganhou!!!!'
         mensagem = resultado(self.nickname,self.pontuacao,situacao)
